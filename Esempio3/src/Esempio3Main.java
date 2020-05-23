805bcbb72b13df2529b4149e4d5909734b6e2da3
Lange99 <c.langellotti@studenti.unibs.it>
2020-05-23 15:45:04 +0200

    eserciziobuggato funzionanante

--- Esempio3/src/Esempio3Main.java
+++ Esempio3/src/Esempio3Main.java
@@ -2,8 +2,19 @@
 public class Esempio3Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String stringa1="Evviva ";
+		String stringa2="Che Bello il ";
+		String stringa3="Programma Arnaldoo ";
+		String primaEseconda = stringa1.concat(stringa2);
+		String frase=primaEseconda.concat(stringa3);
+		
+		//Confornto fra stringhe
+		if(stringa1=="Evviva") {
+			System.out.println("*meme di spiderman che si indica* siamo proprio gemellini");
+		}else {
+			System.out.println("E inveceeeee");
+		}
+		
 	}
 
 }
--- Esercizio3/src/Esercizio3.java
+++ Esercizio3/src/Esercizio3.java
@@ -2,18 +2,18 @@
 public class Esercizio3 {
 
 	public static void main(String[] args) {
-		String stringa1="Evviva";
-		String stringa2="Che Bello il";
-		String stringa3="Programma Arnaldoo";
+		String stringa1="Evviva ";
+		String stringa2="Che Bello il ";
+		String stringa3="Programma Arnaldoo ";
 		String primaEseconda = stringa1.concat(stringa2);
-		String frase=primaEseconda.concat(primaEseconda);
-		String stringa1_copy="Evviva";
+		String frase=primaEseconda.concat(stringa3);
 		
 		//Confornto fra stringhe
-		if(stringa1==stringa1_copy) {
+		if(stringa1=="Evviva") {
 			System.out.println("*meme di spiderman che si indica* siamo proprio gemellini");
 		}else {
 			System.out.println("E inveceeeee");
 		}
+		
 	}
 }
